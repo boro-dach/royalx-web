4c648bfa74f49852b3c966dc2fc0dd57be0e19dc
boro-dach <140444699+boro-dach@users.noreply.github.com>
2026-09-15 23:03:18 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import Footer from "@/widgets/footer/ui/footer";
 import { Providers } from "./providers";
@@ -29,6 +30,12 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased dark`}
     >
+      <head>
+        <Script
+          src="https://telegram.org/js/telegram-web-app.js"
+          strategy="beforeInteractive"
+        />
+      </head>
       <body className="min-h-full flex flex-col">
         <Providers>
           {children} <Footer />
