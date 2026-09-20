74a2e17ca7e73269783249a1166040e64e4f4779
boro-dach <140444699+boro-dach@users.noreply.github.com>
2026-09-20 21:45:29 +0200

    Update footer.tsx

--- widgets/footer/ui/footer.tsx
+++ widgets/footer/ui/footer.tsx
@@ -1,11 +1,11 @@
-"use client"
+"use client";
 
 import { NavLink } from "@/widgets/footer/ui/nav-link";
 import { NAV_ITEMS } from "@/widgets/footer/lib/nav-items";
 
 export default function Footer() {
   return (
-    <footer className="sticky bottom-4 mx-4 mt-auto flex flex-row items-center justify-center gap-2 rounded-full border border-white/10 bg-black/80 px-2 backdrop-blur-md" >
+    <footer className="sticky bottom-4 mx-4 mt-auto flex flex-row items-center justify-center gap-2 rounded-full border border-white/10 bg-transparent px-2 backdrop-blur-xl">
       {NAV_ITEMS.map((item) => (
         <NavLink key={item.href} {...item} />
       ))}
